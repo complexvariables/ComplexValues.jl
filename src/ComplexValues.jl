0eacaca16db8ad95b837fdbeefca9a40ccd93be5
Toby Driscoll <driscoll@udel.edu>
2019-07-24 14:01:03 -0400

    Tidying up

--- src/ComplexValues.jl
+++ src/ComplexValues.jl
@@ -1,6 +1,6 @@
 module ComplexValues
 
-export inf,Polar,Spherical
+export Polar,Spherical
 
 # Individually overloaded operators
 import Base: Complex,complex,float,iszero,isapprox,isinf,isfinite,one,zero
@@ -8,40 +8,31 @@ import Base: +,-,*,/,sign,inv,angle,abs,abs2,real,imag,conj,show
 
 # Utilities
 cleanangle(θ) = π - mod(π-θ,2π)  # map angle to equivalent in (-pi,pi]
-Float = typeof(1.)  # default base type 
+Float = typeof(1.)  # default base floating type 
 
 # Definitions of the types
 include("spherical.jl")
 include("polar.jl")
-include("plotrecipes.jl")
 
-AbstractComplex{T<:AbstractFloat} = Union{Complex{T},Polar{T},Spherical{T}}
-AbstractNonnative{T<:AbstractFloat} = Union{Polar{T},Spherical{T}}
-#AllValues = Union{Number,AbstractNonnative}
+AnyComplex{T<:AbstractFloat} = Union{Complex{T},Polar{T},Spherical{T}}
+AnyNonnative{T<:AbstractFloat} = Union{Polar{T},Spherical{T}}
+#AllValues = Union{Number,AnyNonnative}
 
-# +(u::AllValues,v::AllValues) = +(promote(u,v)...)
-# -(u::AllValues,v::AllValues) = -(promote(u,v)...)
-# *(u::AllValues,v::AllValues) = *(promote(u,v)...)
-# /(u::AllValues,v::AllValues) = /(promote(u,v)...)
-# /(u::AllValues,v::AllValues,args...) = isapprox(promote(u,v)...,args...)
-#one(z::AbstractNonnative) = one(typeof(z))
-#zero(z::AbstractNonnative) = zero(typeof(z))
-inf(z::AbstractNonnative) = inf(typeof(z))
-complex(z::AbstractNonnative) = z
-float(z::AbstractNonnative) = z
+complex(z::AnyNonnative) = z
+float(z::AnyNonnative) = z
 
 # promotion rules and conversion boilerplate
 import Base: promote_rule
 promote_rule(::Union{Type{Complex{S}},Type{S}},::Type{Spherical{T}}) where {S<:Real,T<:AbstractFloat} = Spherical{promote_type(S,T)}
 promote_rule(::Union{Type{Complex{S}},Type{S}},::Type{Polar{T}}) where {S<:Real,T<:AbstractFloat} = Polar{promote_type(S,T)}
-promote_rule(::Type{Polar{S}},::Type{Spherical{T}}) where {S<:AbstractFloat,T<:AbstractFloat} = Polar{promote_type(S,T)}
+promote_rule(::Type{Polar{S}},::Type{Spherical{T}}) where {S<:AbstractFloat,T<:AbstractFloat} = Spherical{promote_type(S,T)}
 
 # convert() boilerplate to invoke constructors
 import Base.convert
-convert(::Type{Complex{S}},z::AbstractNonnative) where S<:Real = convert(Complex{S},Complex(z))
-convert(::Type{Polar{S}},z::AbstractNonnative) where S<:AbstractFloat = Polar{S}(z)
+convert(::Type{Complex{S}},z::AnyNonnative) where S<:Real = convert(Complex{S},Complex(z))
+convert(::Type{Polar{S}},z::AnyNonnative) where S<:AbstractFloat = Polar{S}(z)
 convert(::Type{Polar{S}},z::Number) where S<:AbstractFloat = Polar{S}(Complex(z))
-convert(::Type{Spherical{S}},z::AbstractNonnative) where S<:AbstractFloat = Spherical{S}(z)
+convert(::Type{Spherical{S}},z::AnyNonnative) where S<:AbstractFloat = Spherical{S}(z)
 convert(::Type{Spherical{S}},z::Number) where S<:AbstractFloat = Spherical{S}(Complex(z))
 
 # Most other 1-argument and 2-argument base functions just get converted to regular complex
@@ -50,15 +41,17 @@ for f in [:cos,:sin,:tan,:sec,:csc,:cot,:acos,:asin,:atan,:asec,:acsc,:acot,:sin
 	:exp,:exp10,:exp2,:expm1,:log,:log10,:log1p,:log2,:sqrt]
 	quote
 		import Base: $f
-		$f(z::AbstractNonnative) = $f(Complex(z))
+		$f(z::AnyNonnative) = $f(Complex(z))
 	end |> eval
 end
 
 for f in [:log,:^]
 	quote
 		import Base: $f
-		$f(z::AbstractNonnative,w::AbstractNonnative) = $f(Complex(z),Complex(w))
+		$f(z::AnyNonnative,w::AnyNonnative) = $f(Complex(z),Complex(w))
 	end |> eval
 end
+
+include("plotrecipes.jl")
 
 end # module
