dda54c53deb38ed5a6760000dbfe64c97526c7e9
Toby Driscoll <driscoll@udel.edu>
2023-07-30 16:03:53 -0400

    Update formatting

--- src/ComplexValues.jl
+++ src/ComplexValues.jl
@@ -1,14 +1,14 @@
 module ComplexValues
 
-export Polar,Spherical,S2coord
+export Polar, Spherical, S2coord
 
 # Individually overloaded operators
-import Base: Complex,complex,float,iszero,isapprox,isinf,isfinite,one,zero
-import Base: +,-,*,/,sign,inv,angle,abs,abs2,real,imag,conj,show
+import Base: Complex, complex, float, iszero, isapprox, isinf, isfinite, one, zero
+import Base: +, -, *, /, sign, inv, angle, abs, abs2, real, imag, conj, show
 
 # Utilities
-cleanangle(θ) = π - mod2pi(π-θ)  # map angle to equivalent in (-pi,pi]
-Float = typeof(1.)  # default base floating type 
+cleanangle(θ) = π - mod2pi(π - θ)  # map angle to equivalent in (-pi,pi]
+Float = typeof(1.0)  # default base floating type
 
 # Definitions of the types
 include("spherical.jl")
@@ -23,33 +23,34 @@ float(z::AnyNonnative) = z
 
 # promotion rules and conversion boilerplate
 import Base: promote_rule
-promote_rule(::Union{Type{Complex{S}},Type{S}},::Type{Spherical{T}}) where {S<:Real,T<:AbstractFloat} = Spherical{promote_type(S,T)}
-promote_rule(::Union{Type{Complex{S}},Type{S}},::Type{Polar{T}}) where {S<:Real,T<:AbstractFloat} = Polar{promote_type(S,T)}
-promote_rule(::Type{Polar{S}},::Type{Spherical{T}}) where {S<:AbstractFloat,T<:AbstractFloat} = Spherical{promote_type(S,T)}
+promote_rule(::Union{Type{Complex{S}},Type{S}}, ::Type{Spherical{T}}) where {S<:Real,T<:AbstractFloat} = Spherical{promote_type(S, T)}
+promote_rule(::Union{Type{Complex{S}},Type{S}}, ::Type{Polar{T}}) where {S<:Real,T<:AbstractFloat} = Polar{promote_type(S, T)}
+promote_rule(::Type{Polar{S}}, ::Type{Spherical{T}}) where {S<:AbstractFloat,T<:AbstractFloat} = Spherical{promote_type(S, T)}
 
 # convert() boilerplate to invoke constructors
 import Base.convert
-convert(::Type{Complex{S}},z::AnyNonnative) where S<:Real = convert(Complex{S},Complex(z))
-convert(::Type{Polar{S}},z::AnyNonnative) where S<:AbstractFloat = Polar{S}(z)
-convert(::Type{Polar{S}},z::Number) where S<:AbstractFloat = Polar{S}(Complex(z))
-convert(::Type{Spherical{S}},z::AnyNonnative) where S<:AbstractFloat = Spherical{S}(z)
-convert(::Type{Spherical{S}},z::Number) where S<:AbstractFloat = Spherical{S}(Complex(z))
+convert(::Type{Complex{S}}, z::AnyNonnative) where {S<:Real} = convert(Complex{S}, Complex(z))
+convert(::Type{Polar{S}}, z::AnyNonnative) where {S<:AbstractFloat} = Polar{S}(z)
+convert(::Type{Polar{S}}, z::Number) where {S<:AbstractFloat} = Polar{S}(Complex(z))
+convert(::Type{Spherical{S}}, z::AnyNonnative) where {S<:AbstractFloat} = Spherical{S}(z)
+convert(::Type{Spherical{S}}, z::Number) where {S<:AbstractFloat} = Spherical{S}(Complex(z))
 
 # Most other 1-argument and 2-argument base functions just get converted to regular complex
-for f in [:cos,:sin,:tan,:sec,:csc,:cot,:acos,:asin,:atan,:asec,:acsc,:acot,:sincos,:sinpi,
-	:cosh,:sinh,:tanh,:sech,:csch,:coth,:acosh,:asinh,:atanh,:asech,:acsch,:acoth,
-	:exp,:exp10,:exp2,:expm1,:log,:log10,:log1p,:log2,:sqrt]
-	quote
-		import Base: $f
-		$f(z::AnyNonnative) = $f(Complex(z))
-	end |> eval
+for f in [:cos, :sin, :tan, :sec, :csc, :cot, :acos, :asin, :atan, :asec, :acsc, :acot, :sincos, :sinpi,
+    :cosh, :sinh, :tanh, :sech, :csch, :coth, :acosh, :asinh, :atanh, :asech, :acsch, :acoth,
+    :exp, :exp10, :exp2, :expm1, :log, :log10, :log1p, :log2, :sqrt]
+    quote
+        import Base: $f
+        $f(z::AnyNonnative) = $f(Complex(z))
+    end |> eval
 end
 
-for f in [:log,:^]
-	quote
-		import Base: $f
-		$f(z::AnyNonnative,w::AnyNonnative) = $f(Complex(z),Complex(w))
-	end |> eval
+# Auto-complexify log and power functions (overrides default errors):
+for f in [:log, :^]
+    quote
+        import Base: $f
+        $f(z::AnyNonnative, w::AnyNonnative) = $f(Complex(z), Complex(w))
+    end |> eval
 end
 
 include("plotrecipes.jl")
--- src/plotrecipes.jl
+++ src/plotrecipes.jl
@@ -1,42 +1,42 @@
 using RecipesBase
 
-@recipe function f(z::Array{Polar{T}}) where T
+@recipe function f(z::Array{Polar{T}}) where {T}
     projection --> :polar
-    angle.(z),abs.(z)
+    angle.(z), abs.(z)
 end
 
-@recipe function f(z::Array{Spherical{T}};sphere=true) where T
-    delete!(plotattributes,:sphere) 
+@recipe function f(z::Array{Spherical{T}}; sphere=true) where {T}
+    delete!(plotattributes, :sphere)
     markersize --> 1
     aspect_ratio --> 1
-    xlims --> (-1,1)
-    ylims --> (-1,1)
-    zlims --> (-1,1)
+    xlims --> (-1, 1)
+    ylims --> (-1, 1)
+    zlims --> (-1, 1)
 
     @series begin
-        x = [ cos(z.lat)*cos(z.lon) for z in z ]
-        y = [ cos(z.lat)*sin(z.lon) for z in z ]
-        z = [ sin(z.lat) for z in z ]
-        x,y,z
+        x = [cos(z.lat) * cos(z.lon) for z in z]
+        y = [cos(z.lat) * sin(z.lon) for z in z]
+        z = [sin(z.lat) for z in z]
+        x, y, z
     end
 
-    function latcurves(n) 
-        lats = π*(1-n:2:n-1)/(2n+2)
-        ϕ = π*(-200:200)/200
-        [(cos(θ)*cos.(ϕ),cos(θ)*sin.(ϕ),fill(sin(θ),length(ϕ))) for θ in lats]
+    function latcurves(n)
+        lats = π * (1-n:2:n-1) / (2n + 2)
+        ϕ = π * (-200:200) / 200
+        [(cos(θ) * cos.(ϕ), cos(θ) * sin.(ϕ), fill(sin(θ), length(ϕ))) for θ in lats]
     end
-    
-    function loncurves(n) 
-        θ = π*(-100:100)/200
-        longs = 2π*(0:n-1)/n
-        [(cos.(θ)*cos(ϕ),cos.(θ)*sin(ϕ),sin.(θ)) for ϕ in longs]
-    end    
 
-    if isa(sphere,Tuple) 
-        nlat,nlon = sphere
-        sphere = true 
-    elseif isa(sphere,Int)
-        nlat = nlon = sphere 
+    function loncurves(n)
+        θ = π * (-100:100) / 200
+        longs = 2π * (0:n-1) / n
+        [(cos.(θ) * cos(ϕ), cos.(θ) * sin(ϕ), sin.(θ)) for ϕ in longs]
+    end
+
+    if isa(sphere, Tuple)
+        nlat, nlon = sphere
+        sphere = true
+    elseif isa(sphere, Int)
+        nlat = nlon = sphere
         sphere = true
     else
         nlon = 12
@@ -53,7 +53,7 @@ end
                 markershape := :none
                 linewidth := 0.5
                 label := ""
-                c[1],c[2],c[3]
+                c[1], c[2], c[3]
             end
         end
 
@@ -67,7 +67,7 @@ end
                 markershape := :none
                 linewidth := 0.5
                 label := ""
-                c[1],c[2],c[3]
+                c[1], c[2], c[3]
             end
         end
     end
