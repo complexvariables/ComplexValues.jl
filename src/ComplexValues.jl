1594c2ccae0c878646ca1249d278f2e0a0b1dd03
Toby Driscoll <driscoll@udel.edu>
2023-08-23 15:28:41 -0400

    remove plotting

--- src/ComplexValues.jl
+++ src/ComplexValues.jl
@@ -53,11 +53,4 @@ for f in [:log, :^]
     end |> eval
 end
 
-include("plotrecipes.jl")
-
-using Requires
-function __init__()
-	@require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("makie.jl")
-end
-
 end # module
